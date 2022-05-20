84cd40be68f8fad716e3aeabb0f5575eb7b35010
AllenXie <89894318+Allenqiyang@users.noreply.github.com>
2022-05-20 22:43:27 +0800

    Add files via upload

---
+++ react_style/src/App.js
@@ -0,0 +1,36 @@
+import React, { PureComponent } from 'react'
+
+import classNames from 'classnames'
+
+export default class App extends PureComponent {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isActive: true
+    }
+  }
+
+  render() {
+    const {isActive} = this.state
+    const isFoo = false
+    return (
+      <div>
+        {/* 原生方法 */}
+        <h3 className={"foo bar active title"}>I'm title1</h3>
+        <h3 className={"title" + (isActive ? " active" : "")}>I'm title2</h3>
+        <h3 className={["title", (isActive ? "active" : "")].join(" ")}>I'm title3</h3>
+
+        {/* classnames方法 */}
+        <h3 className='foo bar active title'>I'm title</h3>
+        <h3 className={classNames("foo", "bar", "active", "title")}>I'm title</h3>
+        <h3 className={classNames({"active": isActive, "foo": isFoo})}>I'm title</h3>
+        <h3>I'm title</h3>
+        <h3>I'm title</h3>
+        <h3>I'm title</h3>
+      </div>
+    )
+  }
+}
+
+
