a3d6e7c206617681203cb49f1225cbe21286da5a
AllenXie <89894318+Allenqiyang@users.noreply.github.com>
2022-05-25 22:32:44 +0800

    Add files via upload

---
+++ react_style/src/App.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+import CSStransition from "./transition/CSStransition";
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        <CSStransition/>
+      </div>
+    )
+  }
+}
