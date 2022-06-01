a173cddabd583c6bd9a037140507d6f3437154b1
AllenXie <89894318+Allenqiyang@users.noreply.github.com>
2022-06-01 23:18:53 +0800

    Add files via upload

--- react_redux/src/store/reducer.js
+++ react_redux/src/store/reducer.js
@@ -1,12 +1,11 @@
 import { add, sub, inc, dec, change_b, change_r } from './constant.js'
 
-const initialState = {
-  counter: 0,
-  banners: [],
-  recommends: []
+const countInitialState = {
+  counter: 0
 }
 
-function reducer(state = initialState, action) {
+// 拆分count的reducer
+function countReducer(state = countInitialState, action) {
   switch (action.type) {
     case add:
       return { ...state, counter: state.counter + action.num }
@@ -16,12 +15,32 @@ function reducer(state = initialState, action) {
       return { ...state, counter: state.counter + 1 }
     case dec:
       return { ...state, counter: state.counter - 1 }
+    default:
+      return state
+  }
+}
+
+const homeInitialState = {
+  banners: [],
+  recommends: []
+}
+
+// 拆分homeReducer
+function homeReducer(state = homeInitialState, action) {
+  switch (action.type) {
     case change_b:
       return { ...state, banners: action.banners}
     case change_r:
       return { ...state, recommends: action.recommends}
     default:
       return state
+  }
+}
+
+function reducer(state = {}, action) {
+  return {
+    counterInfo: countReducer(state.counterInfo, action),
+    homeInfo: homeReducer(state.homeInfo, action)
   }
 }
 
